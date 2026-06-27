6cf8e5eb3dc21c0df3ecf354b6f02bf08cc834b5
Iqra <Iqra.baig1604@outlook.com>
2026-06-27 21:15:20 +0530

    Initial project setup: Spring Boot + React

---
+++ treasury-backend/src/main/java/com/treasuryflow/treasury_backend/TreasuryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.treasuryflow.treasury_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasuryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasuryBackendApplication.class, args);
+	}
+
+}
